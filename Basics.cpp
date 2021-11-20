2a30b4a9159afc381510d6ec70ef205ae7779c34
Anshika-tech <srivastavanshika10@gmail.com>
2021-11-20 10:17:14 +0530

    from Scratch

---
+++ Basics.cpp
@@ -0,0 +1,14 @@
+//basic syntax
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,r,s=0;
+    cin>>n;
+    while(n>0){
+        r=n%10;
+        s=s*10+r;
+        n=n/10;
+    }
+    cout<<s<<endl;
+    return 0;
+}

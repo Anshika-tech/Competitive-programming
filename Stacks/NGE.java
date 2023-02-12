50f7f73a98114abb073fb51103a6f0084616a393
Anshika-tech <srivastavanshika10@gmail.com>
2023-02-12 18:13:13 +0530

    Next Greater element and next smaller element

--- Stacks/NGE.java
+++ Stacks/NGE.java
@@ -17,6 +17,24 @@ public class NGE{
           System.out.print(ans[i]+" ");
        }
     } 
+
+    public static void nextSmallerElement(long[] arr, int n)
+    { 
+          long[] ans=new long[n];
+        Stack<Integer>s=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!s.isEmpty()&&arr[i]<arr[s.peek()]){
+                ans[s.pop()]=arr[i];
+            }
+            s.push(i);
+        }
+        while(!s.isEmpty()){
+            ans[s.pop()]=-1;
+        }
+       for(int i=0;i<n;i++){
+          System.out.print(ans[i]+" ");
+       }
+    } 
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
@@ -24,6 +42,10 @@ public class NGE{
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
+        System.out.println("Next Greater Element");
         nextLargerElement(arr,n);
+        System.out.println("Next Smaller Element");
+        nextSmallerElement(arr,n);
+
     }
 }
